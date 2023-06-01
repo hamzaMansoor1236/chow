45eb8d973734576374932d4389dab8e5d98c5a1f
Hamza Mansoor <102830795+hamzaMansoor1236@users.noreply.github.com>
2023-06-01 17:57:12 +0500

    first commit

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,36 +1,26 @@
-import React, { useEffect, useState }  from 'react'
-import Hero from "./assets/images/Hero.png";
-import Searchform from "./assets/images/Searchform.png"
-import Section4 from "./assets/images/Section4.png"
-import Footer from "./assets/images/Footer.png"
-import Section1 from "./assets/images/Section1.png"
-import Section5 from "./assets/images/Section5.png"
-import Section2 from "./assets/images/dont worries.png"
+import React, { useEffect, useState } from 'react'
+import header from "./assets/images/Header.png";
+import section2 from "./assets/images/section2.png";
+import section3 from "./assets/images/section3.png";
+import footer from "./assets/images/footer.png";
+import section4 from "./assets/images/section4.png";
 
 function LandingPage() {
 
-    
-  return (
-    <div>
 
-        <div>
-            {/* <img src={Hero} alt='img' style={{width:'100%',height:'80vh',objectFit:"fill"}} /> */}
-            <img src={Section1 } alt='img'  style={{width:'100%',height:'100vh',objectFit:"fill"}} />
-        </div>
-        <div>
-            <img src={Section2} alt=""   style={{width:'100%',height:'200vh',objectFit:"fill"}}  />
-        </div>
-        <div>
-        <img src={Section4} alt='img'   style={{width:'100%',marginTop:'100px'}} />
-        </div>
-        <div>
-        <img src={Section5} alt='img'  style={{width:'100%',height:'500vh',objectFit:'fill',marginTop:'-4px'}} />
-        </div>
-        <div>
-        <img src={Footer} alt='img'   style={{width:'100%',height:'100vh',paddingTop:'100px'}} />
-        </div>
-      
+  return (
+    <div style={{ background: 'rgb(243,255,249)' }}>
+
+
+      <img src={header} alt="" style={{ width: "100%", verticalAlign: "top" }} />
+      <div style={{ width: '1880px', paddingLeft: '117px' }}>
+        <img src={section2} alt="" style={{ width: "100%", verticalAlign: "top" }} />
+        <img src={section4} alt="" style={{ width: "100%", verticalAlign: "top", marginTop: '-2px' }} />
+      </div>
+      <img src={section3} alt="" style={{ width: "100%", verticalAlign: "top" }} />
+      <img src={footer} alt="" style={{ width: "100%", verticalAlign: "top" }} />
     </div>
+
   )
 }
 
